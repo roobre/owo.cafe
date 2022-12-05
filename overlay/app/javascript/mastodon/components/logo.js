98828c452e414d4f4a4660df8c2bad06c35f2f4f
Roberto Santalla <roobre@roobre.es>
2022-12-05 11:33:27 +0100

    logos: update to latest owo-tan version

--- overlay/app/javascript/mastodon/components/logo.js
+++ overlay/app/javascript/mastodon/components/logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-    <svg viewBox='0 0 365 108' className='logo' role='img'>
+    <svg viewBox="0 0 365.28 108.24" className='logo' role='img'>
         <title>owo.cafe</title>
         <use xlinkHref='#logo-symbol-wordmark' />
     </svg>
